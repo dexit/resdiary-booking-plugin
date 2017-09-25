c138b39045d38e655eebe53300b55ecbdda6cc1a
nevace <liam.neville89@gmail.com>
2017-09-25 12:46:12 +0100

    fixed hot reloading

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import {render} from 'react-dom';
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from './store';
+import {AppContainer} from 'react-hot-loader'
 import ResDiaryBookingPlugin from './ResDiaryBookingPlugin';
 import './assets/styles/_p-form.scss';
 
 const container = document.getElementById('resdiary-reservations');
 
-if (module.hot) {
-  module.hot.accept();
-}
-
 render(
-  <Provider store={store}>
-    <ResDiaryBookingPlugin {...container.dataset} />
-  </Provider>,
+  <AppContainer>
+    <Provider store={store}>
+      <ResDiaryBookingPlugin {...container.dataset} />
+    </Provider>
+  </AppContainer>,
   container
 );
+
+if (module.hot) {
+  module.hot.accept('./ResDiaryBookingPlugin', () => {
+    render(
+      <AppContainer>
+        <Provider store={store}>
+          <ResDiaryBookingPlugin {...container.dataset} />
+        </Provider>
+      </AppContainer>,
+      container
+    );
+  });
+}
