c3f239d00a6d781c0cbad993933c8266b0a28202
nevace <liam.neville89@gmail.com>
2017-10-04 22:38:26 +0100

    add styles, fix people dropdown on changedate

--- src/routes/ReservationDetails/index.js
+++ src/routes/ReservationDetails/index.js
@@ -11,7 +11,7 @@ class ReservationDetails extends Component {
 
   state = {
     selectedDay: (this.props.booking.VisitDate && new Date(this.props.booking.VisitDate)) || (this.props.timeSlot.time && new Date(this.props.timeSlot.time)) || null,
-    peopleValue: (this.props.booking.PartySize && this.props.booking.PartySize) || null,
+    peopleValue: (this.props.booking.PartySize && this.props.booking.PartySize) || (this.props.reservationDetails && this.props.reservationDetails.values && this.props.reservationDetails.values.people) || null,
     sittingValue: (this.props.reservationDetails && this.props.reservationDetails.values && this.props.reservationDetails.values.sitting) || null,
     tabIndex: this.props.availability.length ? 1 : 0,
     resetForm: !Object.keys(this.props.booking).length
